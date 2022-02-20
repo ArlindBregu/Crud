b0fac882b61fd1f695237e6871c5836e2abe33e7
Arlind <arlindbregu10@gmail.com>
2022-02-20 22:42:18 +0100

    secondo commit

--- javascript/function.js
+++ javascript/function.js
@@ -5,7 +5,6 @@ var data=[
     "firstName": "Georgi",
     "lastName": "Facello",
     "gender": "M",
-    "hireDate": "1986-06-25",
   },
   {
     "id": 10002,
@@ -13,7 +12,6 @@ var data=[
     "firstName": "Bezalel",
     "lastName": "Simmel",
     "gender": "F",
-    "hireDate": "1985-11-20",
   },
   {
     "id": 10003,
@@ -21,7 +19,6 @@ var data=[
     "firstName": "Parto",
     "lastName": "Bamford",
     "gender": "M",
-    "hireDate": "1986-08-27T22:00:00.000+0000",
   },
   {
     "id": 10004,
@@ -29,8 +26,6 @@ var data=[
     "firstName": "Chirstian",
     "lastName": "Koblick",
     "gender": "M",
-    "hireDate": "1986-11-30",
-
   },
   {
     "id": 10005,
@@ -38,14 +33,69 @@ var data=[
     "firstName": "Kyoichi",
     "lastName": "Maliniak",
     "gender": "M",
-    "hireDate": "1989-09-11T22:00:00.000+0000",
-
   }
 ];
 var nextId = 10006;
 
 $(document).ready(function() {
+  displayEmlpoyeeList();
 
-    
+  $("#create-employee-form").on('submit', function(e) {
+    e.preventDefault();
+    var form_action = $("#create-employee-form").attr("action");
+    var name = $("#name").val();
+    var surname = $("#surname").val();
+    var birth = $("#birthdate").val();
+    var sex = $("#gender").val()
+    dysplay = "create";
 
-})
+    if(name != '' && surname != ''){
+      data.push({id: nextId, birthDate: birth, firstName: name, lastName: surname, gender: sex});
+      nextId++;
+      displayEmlpoyeeList();
+      $("#create-employee").modal('hide');
+      toastr.success('Employee created successfully.', 'Success Alert', {timeOut:5000});
+    }else{
+      alert('All fields are required. Please make sure you fill out all fields correctly.')
+    }
+  });
+
+  /*$("body").on("click", "edit-employee", function() {
+    var id = $(this).parent("td").data('id');
+    var birthDate = $(this).parent("td").prev("td").text();
+    var firstName = $(this).parent("td").prev("td").prev("td").text();
+    var lastName = $(this).parent("td").prev("td").prev("td").prev("td").text();
+    var gender = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").text();
+
+
+  })*/
+
+  $("body").on("click", ".delete-employee", function() {
+    var id = $(this).parent("td").data('id');
+    for(let i=0; i<data.length; i++){
+      if(data[i].id==id){
+        data.splice(i, 1);
+      }
+    }
+    displayEmlpoyeeList();
+  });
+
+  function displayEmlpoyeeList() {
+    var rows = '';
+    $.each(data, function(index, value) {
+      rows = rows + '<tr>';
+      rows= rows +'<td>' +value.id +'</td>';
+      rows= rows +'<td>' +value.firstName +'</td>';
+      rows= rows +'<td>' +value.lastName +'</td>';
+      rows= rows +'<td>' +value.birthDate +'</td>';
+      rows= rows +'<td>' +value.gender +'</td>';
+      rows= rows +'<td data-id="'+value.id+'">';
+      rows = rows +'<button class="btn btn-danger btn-sm delete-employee">Elimina</button>';
+      rows = rows +'</td>';
+      rows = rows +'</tr>';
+    });
+    $("tbody").html(rows);
+  }
+
+});
+
