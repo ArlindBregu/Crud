570e8e9a89d1c3678b4240f99eb1ebcb384395f7
lai2-01 <lai2-01@itismeucci.com>
2022-02-28 11:45:17 +0100

    quinto commit

--- javascript/function.js
+++ javascript/function.js
@@ -1,54 +1,51 @@
-var data=[
-    {
-    "id": 10001,
-    "birthDate": "1953-09-01",
-    "firstName": "Georgi",
-    "lastName": "Facello",
-    "gender": "M",
-  },
-  {
-    "id": 10002,
-    "birthDate": "1964-06-01",
-    "firstName": "Bezalel",
-    "lastName": "Simmel",
-    "gender": "F",
-  },
-  {
-    "id": 10003,
-    "birthDate": "1959-12-02",
-    "firstName": "Parto",
-    "lastName": "Bamford",
-    "gender": "M",
-  },
-  {
-    "id": 10004,
-    "birthDate": "1954-04-30",
-    "firstName": "Chirstian",
-    "lastName": "Koblick",
-    "gender": "M",
-  },
-  {
-    "id": 10005,
-    "birthDate": "1955-01-20",
-    "firstName": "Kyoichi",
-    "lastName": "Maliniak",
-    "gender": "M",
-  }
-];
-var nextId = 10006;
+var data=[];
+var links;
+var page = 0;
+var api = "http://localhost:8080/employees?page="+page+"&size=20";
+var nextId = 500000;
 
 $(document).ready(function() {
   
-  $.ajax({
-    method: "GET",
-    url: "http://localhost:8080/employees"
-  })
-  .done(function(msg) {
-    console.log(msg['_embedded']['employees']);
-    data=msg['_embedded']['employees'];
-    displayEmlpoyeeList();
+  get();
+
+  $("#first-button").on("click", function() {
+    api=links['first']['href'];
+    get();
   });
 
+  $("#next-button").on("click", function() {
+    api = links['next']['href'];
+    page++;
+    get();
+  });
+
+  $("#previous-button").on("click", function() {
+    page--;
+    api = "http://localhost:8080/employees?page="+page+"&size=20";
+    get();
+  });
+
+  $("#last-button").on("click", function() {
+    api=links['last']['href'];
+    get();
+  })
+
+
+  function get() {
+    $.ajax({
+      method: "GET",
+      url: api
+    })
+    .done(function(msg) {
+      console.log(msg['_embedded']['employees']);
+      data=msg['_embedded']['employees'];
+      links=msg['_links'];
+      displayEmlpoyeeList();
+    });
+  }
+  
+
+  //Add an employee
   $("#create-employee-form").on('submit', function(e) {
     e.preventDefault();
     var form_action = $("#create-employee-form").attr("action");
@@ -61,7 +58,9 @@ $(document).ready(function() {
     if(name != '' && surname != ''){
       data.push({id: nextId, birthDate: birth, firstName: name, lastName: surname, gender: sex});
       nextId++;
+  
       displayEmlpoyeeList();
+      $("#create-employee-form")[0].reset();
       $("#create-employee").modal('hide');
       toastr.success('Employee created successfully.', 'Success Alert', {timeOut:5000});
     }else{
@@ -69,6 +68,7 @@ $(document).ready(function() {
     }
   });
 
+  //Edit an employee
   $("body").on("click", ".edit-employee", function() {
     var id = $(this).parent("td").data('id');
     var gender = $(this).parent("td").prev("td").text();
@@ -80,19 +80,21 @@ $(document).ready(function() {
     $("#name_edit").val(firstName);
     $("#surname_edit").val(lastName);
     $("#gender_edit").val(gender);
+    $("#edit-employee-form").find(".edit-id").val(id);
 
     $("#edit-employee-form").on('submit', function(e) {
       e.preventDefault();
-      var form_action = $("#edit-employee-form").attr("action");
+      //var form_action = $("#edit-employee-form").attr("action");
+      var idE = $("#id_edit").val();
       var name = $("#name_edit").val();
       var surname = $("#surname_edit").val();
       var birth = $("#birth_edit").val();
       var sex = $("#gender_edit").val()
-      dysplay = "create";
+      //dysplay = "create";
 
       if(firstName != '' && lastName != ''){
         for(let i=0; i<data.length; i++){
-          if(data[i].id==id){
+          if(data[i].id==idE){
             data[i].firstName= name;
             data[i].lastName= surname;
             data[i].birthDate = birth;
@@ -109,6 +111,7 @@ $(document).ready(function() {
     })
   })
 
+  //Delete an employee
   $("body").on("click", ".delete-employee", function() {
     var id = $(this).parent("td").data('id');
     for(let i=0; i<data.length; i++){
@@ -130,9 +133,9 @@ $(document).ready(function() {
       rows= rows +'<td>' +value.birthDate +'</td>';
       rows= rows +'<td>' +value.gender +'</td>';
       rows= rows +'<td data-id="'+value.id+'">';
-      rows = rows +'<button class="btn btn-danger btn-sm delete-employee">Elimina</button>';
+      rows = rows +'<button class="btn btn-danger btn-sm delete-employee"><i class="fa-solid fa-trash-can"></i></button>';
       rows = rows +'&nbsp&nbsp';
-      rows = rows +'<button class="btn btn-secondary btn-sm edit-employee" data-toggle="modal" data-target="#edit-employee">Modifica</button>';
+      rows = rows +'<button class="btn btn-secondary btn-sm edit-employee" data-toggle="modal" data-target="#edit-employee"><i class="fa-solid fa-pen"></i></button>';
       rows = rows +'</td>';
       rows = rows +'</tr>';
     });
